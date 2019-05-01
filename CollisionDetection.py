217a93a7c7f55fdc60f173da9c6d7f2713795b14
mohamedelgendyx <48632847+mohamedelgendyx@users.noreply.github.com>
2019-05-01 14:05:36 +0200

    Update CollisionDetection.py

--- CollisionDetection.py
+++ CollisionDetection.py
@@ -1,35 +1,36 @@
-from gameobject import *
 from math import *
-def test_cu (r,center_x,center_y,l,w,ce_x,ce_y):
-    L = l/2
-    W = w/2
-    r_x = ce_x + W
-    #l_x = ce_x
-    l_x = ce_x - W
-    t_y = ce_y + L
-    #b_y = ce_y
-    b_y = ce_y - L
-    if center_x <= l_x:
-        test_x = l_x
+
+def testCube (playerRadius,playerCX,playerCZ,objectLength,objectWidth,objectCX,objectCZ):
+
+    #ObjectDimensions
+    objectLength = objectLength/2
+    objectWidth = objectWidth/2
+    rigthX = objectCX + objectWidth
+    leftX = objectCX - objectWidth
+    topZ = objectCZ + objectLength
+    bottomZ = objectCZ - objectLength
+
+    if playerCX <= leftX:
+         collisionX= leftX
     else:
-        if center_x >= (r_x):
-            test_x = r_x
+        if playerCX >= rigthX:
+            collisionX = rigthX
         else:
-            test_x = center_x
+            collisionX = playerCX
 
-    if center_y <= b_y:
-        test_y = b_y
+    if playerCZ <= bottomZ:
+        collisionZ = bottomZ
     else:
-        if center_y >= (t_y):
-            test_y = t_y
+        if playerCZ >= topZ:
+            collisionZ = topZ
         else:
-            test_y = center_y
+            collisionZ = playerCZ
 
-    dist_x = center_x - test_x
-    dist_y = center_y - test_y
-    distance = sqrt((dist_x * dist_x) + (dist_y * dist_y))
+    distanceX = playerCX - collisionX
+    distanceZ = playerCZ - collisionZ
+    distance = sqrt(distanceX ** 2 + distanceZ ** 2)
 
-    if distance <= r:
+    if distance <= playerRadius:
         return True
     else:
         return False
