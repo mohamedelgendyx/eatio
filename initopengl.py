6df7860ebdd009dd90c01fa33c593f1593499368
mohamedelgendyx <48632847+mohamedelgendyx@users.noreply.github.com>
2019-05-07 09:09:00 +0200

    Update initopengl.py

--- initopengl.py
+++ initopengl.py
@@ -5,6 +5,9 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
+
+# import pygame
+
 def displayFunction():
     global draw
     draw()
@@ -18,7 +21,9 @@ def initOpenGl(drawParameter, onMouseKey, onKeyboardKey, onSpecialKey):
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH)
     glutInitWindowSize(500, 500)
     glutCreateWindow(b"EAT.io")
-
+    # pygame.mixer.init(44100, -16, 2, 2048)  # intianliazion of game sound
+    # pygame.mixer.music.load("/resources/sounds/bg_music.wav")  # open sound file
+    # pygame.mixer.music.play(-1)  # play sound to infint
     PlaySound("resources/sounds/bg_music.wav", SND_FILENAME | SND_LOOP | SND_ASYNC)
 
     currentTime = time()
