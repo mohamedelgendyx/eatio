13b2783b3f77568722e770aa65ab80e904d9af84
mohamedelgendyx <48632847+mohamedelgendyx@users.noreply.github.com>
2019-05-07 09:06:03 +0200

    Update gamestatus.py

--- gamestatus.py
+++ gamestatus.py
@@ -1,30 +1,13 @@
-#from game import currentTime
-from objects.PlayerObject import *
-from gametextures import  drawText2D
-
-player = PlayerObject()
 GAME_STATUS_MAINMENU = 0
 GAME_STATUS_COUNTDOWN = 1
 GAME_STATUS_PLAYING = 4
 GAME_STATUS_SCORE = 100
-GAME_SPEED_FACTOR = 1
-AREA_TO_SCORE= 2
-PLAYER_MINIMUM_MOVEMENT = 1
-currentGameScore = 0
-refrenceGameScore =0
-toMakeBiger=10
+GAME_SPEED_FACTOR = .5
 
+# Player
+PLAYER_MINIMUM_SPEED = 7
+PLAYER_MAXIMUM_SPEED = 10
+PLAYER_SPEED_FACTOR = 1.5
 
-def Score_update(currGameScore):
-    #global cameraZoom
-    global refrenceGameScore ,toMakeBiger
-    global currentGameScore
-    if currGameScore - refrenceGameScore >= toMakeBiger :
-        player.radius += player.radius *(1/player.radius)
-        #cameraZoom += 3
-        player.update_area()
-        refrenceGameScore = currGameScore
-        drawText2D("Size Up!", player.posX, 2, player.posZ, scaleFactor=1)
-        toMakeBiger*=3
-        return True
-    return False
+# Score
+AREA_TO_SCORE_RATIO = 2
