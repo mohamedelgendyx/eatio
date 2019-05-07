8a51afe09e928d479df2413588bdc566a53bc3ca
mohamedelgendyx <48632847+mohamedelgendyx@users.noreply.github.com>
2019-05-07 09:03:45 +0200

    Update CollisionDetection.py

--- CollisionDetection.py
+++ CollisionDetection.py
@@ -1,17 +1,25 @@
 from math import *
 
-def testCube (playerRadius,playerCX,playerCZ,objectLength,objectWidth,objectCX,objectCZ):
+def isCollided(player, obj):
+    if obj.rotY == 90 or obj.rotY == -90:
+        return testCube(player.radius, player.posX, player.posZ, obj.width, obj.Length, obj.posX, obj.posZ)
+    else:
+        return testCube(player.radius, player.posX, player.posZ, obj.Length, obj.width, obj.posX, obj.posZ)
 
-    #ObjectDimensions
-    objectLength = objectLength/2
-    objectWidth = objectWidth/2
+
+def testCube(playerRadius, playerCX, playerCZ, objectLength, objectWidth, objectCX, objectCZ):
+    
+    # ObjectDimensions
+
+    objectLength = objectLength / 2
+    objectWidth = objectWidth / 2
     rigthX = objectCX + objectWidth
     leftX = objectCX - objectWidth
     topZ = objectCZ + objectLength
     bottomZ = objectCZ - objectLength
 
     if playerCX <= leftX:
-         collisionX= leftX
+        collisionX = leftX
     else:
         if playerCX >= rigthX:
             collisionX = rigthX
