bc5ad6e78549d177041e0e25cbf802e5888108ef
mohamedelgendyx <48632847+mohamedelgendyx@users.noreply.github.com>
2019-05-07 09:00:52 +0200

    Update CharacterObject.py

--- objects/CharacterObject.py
+++ objects/CharacterObject.py
@@ -2,35 +2,37 @@ from OpenGL.GLUT import *
 from gameobject import *
 from random import randrange
 
+
 class CharacterObject(GameObject):
 
     def __init__(self, posX, posY, posZ, scaleX=1, scaleY=1, scaleZ=1, rotY=0, r=0, g=0, b=0):
         super().__init__(posX, posY, posZ, scaleX, scaleY, scaleZ, rotY)
-        self.Length=0
-        self.width=0
-        self.area=2
+        self.Length = 0
+        self.width = 0
+        self.area = 2
         self.HeadDeltaY = 0
         self.HeadUpDown = 1
-        self.r=r/255
-        self.g=g/255
-        self.b=b/255
+        self.r = r / 255
+        self.g = g / 255
+        self.b = b / 255
+        self.alpha = 1
         Straight(self)
 
     def draw(self):
         self.applyParentTransform()
-        glColor3f(self.r, self.g, self.b)
+        glColor4f(self.r, self.g, self.b, self.alpha)
         glTranslate(0, 1.5, 0)
         glScale(.6, .9, .4)
         glutSolidCube(.976)
 
         self.applyParentTransform()
-        glColor3f(241 / 255, 195 / 255, 125 / 255)
+        glColor4f(241 / 255, 195 / 255, 125 / 255, self.alpha)
         glTranslate(0, 2.0 + self.HeadDeltaY, 0)
         glScale(.6, .3, .4)
         glutSolidCube(1.03)
 
         self.applyParentTransform()
-        glColor3f(0, 0, 0.1)
+        glColor4f(0, 0, 0.1, self.alpha)
         glTranslate(0, 2.25 + self.HeadDeltaY, 0)
         glScale(.6, .2, .4)
         glutSolidCube(1.03)
@@ -46,25 +48,31 @@ class CharacterObject(GameObject):
         else:
             self.HeadDeltaY -= dt * 0.5
 
-#movement
+
+# movement
 def Straight(obj):
-    obj.rotY=360
-    a20 = DeltaAnimation(AnimationParams.posZ, 3 , 3)
+    obj.rotY = 360
+    a20 = DeltaAnimation(AnimationParams.posZ, 3, 3)
     a20.onAnimationFinished = lambda: partX1(obj)
     obj.startAnimation(a20)
+
+
 def partX1(obj):
-    obj.rotY=90
-    a9 = DeltaAnimation(AnimationParams.posX,-3 , 3)
-    a9.onAnimationFinished = lambda : Straight2(obj)
-    obj.startAnimation(a9)
-def partX2(obj):
-    obj.rotY=270
-    a9 = DeltaAnimation(AnimationParams.posX,3 , 3)
-    a9.onAnimationFinished = lambda : Straight(obj)
+    obj.rotY = 90
+    a9 = DeltaAnimation(AnimationParams.posX, -3, 3)
+    a9.onAnimationFinished = lambda: Straight2(obj)
     obj.startAnimation(a9)
 
+
+def partX2(obj):
+    obj.rotY = 270
+    a9 = DeltaAnimation(AnimationParams.posX, 3, 3)
+    a9.onAnimationFinished = lambda: Straight(obj)
+    obj.startAnimation(a9)
+
+
 def Straight2(obj):
-    obj.rotY=180
-    a21 = DeltaAnimation(AnimationParams.posZ,-3 , 3)
+    obj.rotY = 180
+    a21 = DeltaAnimation(AnimationParams.posZ, -3, 3)
     a21.onAnimationFinished = lambda: partX2(obj)
     obj.startAnimation(a21)
