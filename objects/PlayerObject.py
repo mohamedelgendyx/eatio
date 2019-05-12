8a6bf8676eabe88957dc8258164381acee02a940
mohamedelgendyx <48632847+mohamedelgendyx@users.noreply.github.com>
2019-05-12 23:44:55 +0200

    Update PlayerObject.py

--- objects/PlayerObject.py
+++ objects/PlayerObject.py
@@ -2,6 +2,7 @@ import numpy as np
 from math import *
 from random import *
 from gameobject import *
+from gametextures import drawText2D
 
 
 def draw_circle(r=0.5, z=0):
@@ -17,7 +18,8 @@ def draw_circle(r=0.5, z=0):
 class PlayerObject(GameObject):
 
     def __init__(self, posX=randrange(-100, 100), posY=0, posZ=randrange(-100, 0), scaleX=1, scaleY=1, scaleZ=1, rotY=0,
-                 radius=1, r=0, g=1, b=0):
+                 radius=1, r=0, g=1,
+                 b=0):
         super().__init__(posX, posY, posZ, scaleX, scaleY, scaleZ, rotY)
         self.radius = radius
         self.xdis = randrange(-100, 100)
@@ -25,12 +27,16 @@ class PlayerObject(GameObject):
         self.r = r
         self.g = g
         self.b = b
-        self.x = 0
-        self.z = 0
-        self.area = pi * self.radius * self.radius
+        self.arrowPosX = 0
+        self.arrowPosZ = 0
+        self.area = (pi * self.radius * self.radius) / 2
+        self.score = 0
+        self.currentGameScore = 0
+        self.refrenceGameScore = 0
+        self.toMakeBig = 15
 
     def update_area(self):
-        self.area = pi * self.radius * self.radius
+        self.area = pi * self.radius * self.radius * .5
 
     def draw(self):
         self.applyParentTransform()
@@ -42,7 +48,7 @@ class PlayerObject(GameObject):
         draw_circle(self.radius + self.radius * 0.1, -0.02)
 
         glLoadIdentity()
-        glTranslate(self.posX + self.x, 0.05, self.posZ + self.z)
+        glTranslate(self.posX + self.arrowPosX, 0.05, self.posZ + self.arrowPosZ)
         glRotate(self.rotY, 0, 1, 0)
         glBegin(GL_POLYGON)
         glColor(0, 1, 0)
@@ -51,70 +57,82 @@ class PlayerObject(GameObject):
         glVertex(0, 0, .346 + self.radius * 0.1 * 4)
         glEnd()
 
+    def Score_update(self):
+        if self.currentGameScore - self.refrenceGameScore >= self.toMakeBig:
+            self.radius += 1.5
+            self.update_area()
+            self.refrenceGameScore = self.currentGameScore
+            drawText2D("Size Up!", self.posX, 2, self.posZ, scaleFactor=1)
+            self.toMakeBig *= 3
+            # print("He is Bigggg")
+            return True
+        return False
+
 
 others = 3
-list = []
+listOfallPlayers = []
 
 for i in range(others):
-    list.append(PlayerObject())
+    listOfallPlayers.append(PlayerObject())
 
 for i in range(others):
-    list.append(PlayerObject())
-    list[i].posZ = randrange(-100, 0)
-    list[i].posX = randrange(-100, 100)
+    listOfallPlayers.append(PlayerObject())
+    listOfallPlayers[i].posZ = randrange(-100, 0)
+    listOfallPlayers[i].posX = randrange(-100, 100)
 
 
 def draw_others():
     global others
-    global list
+    global listOfallPlayers
     for i in range(others):
-        if list[i].posX == list[i].xdis and list[i].posZ == list[i].zdis:
-            list[i].xdis = randrange(-100, 100)
-            list[i].zdis = randrange(-100, 0)
+        if listOfallPlayers[i].posX == listOfallPlayers[i].xdis and listOfallPlayers[i].posZ == listOfallPlayers[
+            i].zdis:
+            listOfallPlayers[i].xdis = randrange(-100, 100)
+            listOfallPlayers[i].zdis = randrange(-100, 0)
         else:
-            if list[i].posX < list[i].xdis:
-                list[i].posX += .5
-                if list[i].posZ < list[i].zdis:
-                    list[i].posZ += .5
-                    list[i].rotY = 45
-                    list[i].x = list[i].radius + .2
-                    list[i].z = list[i].radius + .2
-                elif list[i].posZ > list[i].zdis:
-                    list[i].posZ -= .5
-                    list[i].rotY = 135
-                    list[i].x = list[i].radius + .2
-                    list[i].z = -list[i].radius - .2
+            if listOfallPlayers[i].posX < listOfallPlayers[i].xdis:
+                listOfallPlayers[i].posX += .5
+                if listOfallPlayers[i].posZ < listOfallPlayers[i].zdis:
+                    listOfallPlayers[i].posZ += .5
+                    listOfallPlayers[i].rotY = 45
+                    listOfallPlayers[i].arrowPosX = listOfallPlayers[i].radius + .2
+                    listOfallPlayers[i].arrowPosZ = listOfallPlayers[i].radius + .2
+                elif listOfallPlayers[i].posZ > listOfallPlayers[i].zdis:
+                    listOfallPlayers[i].posZ -= .5
+                    listOfallPlayers[i].rotY = 135
+                    listOfallPlayers[i].arrowPosX = listOfallPlayers[i].radius + .2
+                    listOfallPlayers[i].arrowPosZ = -listOfallPlayers[i].radius - .2
                 else:
-                    list[i].rotY = 90
-                    list[i].x = list[i].radius + .2
-                    list[i].z = 0
+                    listOfallPlayers[i].rotY = 90
+                    listOfallPlayers[i].arrowPosX = listOfallPlayers[i].radius + .2
+                    listOfallPlayers[i].arrowPosZ = 0
 
-            elif list[i].posX > list[i].xdis:
-                list[i].posX -= .5
-                if list[i].posZ > list[i].zdis:
-                    list[i].posZ -= .5
-                    list[i].rotY = 225
-                    list[i].x = -list[i].radius - .2
-                    list[i].z = -list[i].radius - .2
-                elif list[i].posZ < list[i].zdis:
-                    list[i].posZ += .5
-                    list[i].rotY = -45
-                    list[i].x = -list[i].radius - .2
-                    list[i].z = list[i].radius + .2
+            elif listOfallPlayers[i].posX > listOfallPlayers[i].xdis:
+                listOfallPlayers[i].posX -= .5
+                if listOfallPlayers[i].posZ > listOfallPlayers[i].zdis:
+                    listOfallPlayers[i].posZ -= .5
+                    listOfallPlayers[i].rotY = 225
+                    listOfallPlayers[i].arrowPosX = -listOfallPlayers[i].radius - .2
+                    listOfallPlayers[i].arrowPosZ = -listOfallPlayers[i].radius - .2
+                elif listOfallPlayers[i].posZ < listOfallPlayers[i].zdis:
+                    listOfallPlayers[i].posZ += .5
+                    listOfallPlayers[i].rotY = -45
+                    listOfallPlayers[i].arrowPosX = -listOfallPlayers[i].radius - .2
+                    listOfallPlayers[i].arrowPosZ = listOfallPlayers[i].radius + .2
                 else:
-                    list[i].rotY = - 90
-                    list[i].x = -list[i].radius - .2
-                    list[i].z = 0
+                    listOfallPlayers[i].rotY = - 90
+                    listOfallPlayers[i].arrowPosX = -listOfallPlayers[i].radius - .2
+                    listOfallPlayers[i].arrowPosZ = 0
             else:
-                if list[i].posZ > list[i].zdis:
-                    list[i].posZ -= .5
-                    list[i].rotY = 180
-                    list[i].x = 0
-                    list[i].z = -list[i].radius - .2
-                elif list[i].posZ < list[i].zdis:
-                    list[i].posZ += .5
-                    list[i].rotY = 0
-                    list[i].x = 0
-                    list[i].z = list[i].radius + .2
+                if listOfallPlayers[i].posZ > listOfallPlayers[i].zdis:
+                    listOfallPlayers[i].posZ -= .5
+                    listOfallPlayers[i].rotY = 180
+                    listOfallPlayers[i].arrowPosX = 0
+                    listOfallPlayers[i].arrowPosZ = -listOfallPlayers[i].radius - .2
+                elif listOfallPlayers[i].posZ < listOfallPlayers[i].zdis:
+                    listOfallPlayers[i].posZ += .5
+                    listOfallPlayers[i].rotY = 0
+                    listOfallPlayers[i].arrowPosX = 0
+                    listOfallPlayers[i].arrowPosZ = listOfallPlayers[i].radius + .2
 
-        list[i].draw()
+        listOfallPlayers[i].draw()
